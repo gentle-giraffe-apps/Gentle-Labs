59657278ac86f0e8a4d23dd98129324f59fb8a22
gentle-giraffe-apps <gentlegiraffeapps@gmail.com>
2026-02-01 14:55:59 -0800

    in progress

--- Concurrency/ImageList/ImageList/ImageList.swift
+++ Concurrency/ImageList/ImageList/ImageList.swift
@@ -4,7 +4,20 @@ import Observation
 import SwiftUI
 import UIKit
 
-// Put all code here
+// TaskGroup (dynamic number of tasks)
+//
+// await withTaskGroup(of: ResultType.self) { group in
+//     for item in items {
+//         group.addTask {
+//             await someAsyncCall(item)
+//         }
+//     }
+//
+//     for await result in group {
+//         // collect results
+//     }
+// }
+
 struct ImageModel: Identifiable, Codable, Hashable, Sendable {
     let id: String
     let url: String
@@ -80,8 +93,33 @@ final class ImageListViewModel {
         self.service = service
     }
     // MILESTONE 1: Add a method that just fetches the banner image, call it from the view
+    
     // MILESTONE 2: Change the method in (1) so it fetches the banner and log in parallel
+    
+    // Syntax:
+    // async let (fixed, small number of tasks)
+    //
+    // async let a = someAsyncCall()
+    // async let b = someAsyncCall()
+    // let result = try await (a, b)
+
+    
     // MILESTONE 3: Fetch a RANDOM number of images (between 5 and 15) IN PARALLEL and assign them to `listImages`.
+
+    // Syntax:
+    // TaskGroup (dynamic number of tasks)
+    //
+    // await withTaskGroup(of: ResultType.self) { group in
+    //     for item in items {
+    //         group.addTask {
+    //             await someAsyncCall(item)
+    //         }
+    //     }
+    //
+    //     for await result in group {
+    //         // collect results
+    //     }
+    // }
 }
 
 struct ImageList: View {
